ab5cf041ab510b09ec35aa8d29003a9ed06ecf07
jack <frontend82@mail.ru>
2021-02-13 17:14:58 +0300

    проверка вводимой информации

--- 12/example12.php
+++ 12/example12.php
@@ -1,16 +1,19 @@
 <?php
+  $username = 'admin';
+  $password = 'admin';
+
   if (isset($_SERVER['PHP_AUTH_USER']) &&
-  	  isset($_SERVER['PHP_AUTH_PW']))
+      isset($_SERVER['PHP_AUTH_PW']))
   {
-    echo "Welcome User: " . htmlspecialchars($_SERVER['PHP_AUTH_USER']);
-    echo "<br>";
-    echo "You Password: " . htmlspecialchars($_SERVER['PHP_AUTH_PW']);
-    //var_dump($_SERVER);
+    if ($_SERVER['PHP_AUTH_USER'] === $username &&
+        $_SERVER['PHP_AUTH_PW']   === $password)
+          echo "You are now logged in";
+    else die("Invalid username/password combination");
   }
   else
   {
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
-    die("Please enter your username and password");
+    die ("Please enter your username and password");
   }
 ?>
